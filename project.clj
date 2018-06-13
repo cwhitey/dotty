0eb174e4c7ac1bf596478622581c5c53a76d9a02
Callum White <callumw1991@gmail.com>
2018-06-13 23:37:07 +1000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cwhitey/dotty "0.1.0-SNAPSHOT"
+(defproject cwhitey/dotty "0.1.0"
   :description "Retrieves environment variables from `.env`"
   :url "https://github.com/cwhitey/dotty"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:resource-paths ["test/resources"]}})
+             :dev {:resource-paths ["test/resources"]}}
+  :deploy-repositories [["releases" :clojars]])
