4ee6f22299c55d5da59315050840a7068cac89a2
Callum White <callumw1991@gmail.com>
2018-06-18 23:35:43 +1000

    Test formatting

--- test/dotty/core_test.clj
+++ test/dotty/core_test.clj
@@ -5,29 +5,31 @@
 
 (deftest decoding-env-vars
   (testing "Honour .env format rules"
+    ;; https://github.com/motdotla/dotenv#rules
     (testing "Decode a URL format"
-      (is (= (decode-var "TEST_ME=http://something.org")
-             {"TEST_ME" "http://something.org"})))
+      (is (= {"TEST_ME" "http://something.org"}
+             (decode-var "TEST_ME=http://something.org"))))
 
     (testing "Subsequent `=` are part of env var"
-      (is (= (decode-var "TEST_ME=http://something.org?param=foo")
-             {"TEST_ME" "http://something.org?param=foo"})))
+      (is (= {"TEST_ME" "http://something.org?param=foo"}
+             (decode-var "TEST_ME=http://something.org?param=foo"))))
 
     (testing "Non-existant value yields empty string"
-      (is (= (decode-var "TEST_ME=")
-             {"TEST_ME" ""})))
+      (is (= {"TEST_ME" ""}
+             (decode-var "TEST_ME="))))
+
     (testing "Preserve double quotes"
-      (is (= (decode-var "TEST_ME=\"my quote\"")
-             {"TEST_ME" "\"my quote\""})))
+      (is (= {"TEST_ME" "\"my quote\""}
+             (decode-var "TEST_ME=\"my quote\""))))
 
     (testing "Strip single quotes"
-      (is (= (decode-var "TEST_ME='http://something.org'")
-             {"TEST_ME" "http://something.org"})))
+      (is (= {"TEST_ME" "http://something.org"}
+             (decode-var "TEST_ME='http://something.org'"))))
 
     (testing "Return nil for invalid formats"
-      (is (= (decode-var "=some value") nil))
-      (is (= (decode-var "TEST_ME?value") nil))
-      (is (= (decode-var "") nil))))
+      (is (= nil (decode-var "=some value")))
+      (is (= nil (decode-var "TEST_ME?value")))
+      (is (= nil (decode-var "")))))
 
   (testing "Read env file from root of project"
     (is (pos-int? (count (read-env-file ".env.test"))))
@@ -35,9 +37,9 @@
 
   (testing "Decode a block of env vars"
     (let [vars-str (slurp (io/resource "env-test-1.txt"))]
-      (is (= (decode-vars vars-str)
-             {"USER" "cwhitey"
+      (is (= {"USER" "cwhitey"
               "APP_ENV" "development"
               "VALID_VAR" ""
               "COUNT" "8"
-              "QUOTE" "\"a very famous quote\""})))))
+              "QUOTE" "\"a very famous quote\""}
+             (decode-vars vars-str))))))
